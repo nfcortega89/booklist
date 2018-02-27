b611dc9175a2560f48a41c6064ca258d198f3f01
Niccolo Ortega <nfcortega89@gmail.com>
2018-02-27 11:52:54 -0800

    book detail works

---
+++ src/containers/book-details.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    if (!this.props.activeBook) {
+      return <div>Select a book to get started</div>;
+    }
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <div>{this.props.activeBook.title}</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    activeBook: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
