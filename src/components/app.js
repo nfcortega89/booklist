da36aadb1845b87966f580ddf45594def29c554a
Niccolo Ortega <nfcortega89@gmail.com>
2018-02-27 11:01:09 -0800

    reducer working

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <BookList />
+      </div>
+    );
   }
 }
